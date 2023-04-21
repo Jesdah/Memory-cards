c6ab4fff90f1e381966a5c161e1fad13994bb0ed
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-04-21 10:14:23 +0000

    still trying to get javascript to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
-let cards = document.getElementsByClassName("play-card");
-let cardOne, cardTwo;
-let disableDeck= false;
+const cards =document.querySelectorAll('play-card');
 
 
-function flipCard({target: clickedCard}) {
-   if (cardOne== clickedCard && !disableDeck) {
-    clickedCard.getElementsByClassName("flip")
-   }
+let icons= ['fa-solid fa-image',
+'fa-solid fa-star',
+'fa-solid fa-heart',
+'fa-solid fa-ghost',
+'fa-solid fa-headphones',
+'fa-solid fa-plane',
+'fa-solid fa-bicycle',
+'fa-solid fa-feather'];
+
+// Shufle icons.
+for (let i = 0; i < 100; i++) {
+    const idx1 = Math.floor(Math.random() * icons.length);
+    const idx2 = Math.floor(Math.random() * icons.length);
     
-}
-
-function matchCard () {
-
-}
-
-function shuffleCard () {
-
+    const temp = icons[idx1];
+  icons[idx1] = icons[idx2];
+  icons[idx2] = temp;
 }
