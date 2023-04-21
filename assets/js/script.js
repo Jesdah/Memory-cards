14377a56cf944370181849e713aeec773718b92d
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-04-21 18:01:45 +0000

    add a shufle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-const cards =document.querySelectorAll('play-card');
+const container =document.querySelectorAll('play-card');
+const cardslength = 16;
+const cards = [];
+
+let previousShownCard = undefined;
 
 
 let icons= ['fa-solid fa-image',
@@ -18,4 +22,34 @@ for (let i = 0; i < 100; i++) {
     const temp = icons[idx1];
   icons[idx1] = icons[idx2];
   icons[idx2] = temp;
+}
+console.log(icons);
+for (let i = 0; i < cardslength; i++) {
+  const card = document.getElementsByClassName('side');
+   card.addEventListener('click', () => {
+    if (!card.classList.contains('flip')) {
+      card.classList.add('flip');
+
+      if (!previousShownCard) {
+        previousShownCard = card;
+      } else {
+        const iconOne = previousShownCard.querySelector('i').classList[1];
+
+        const iconTwo = card.querySelector('i').classList[1];
+
+        if (iconOne !== iconTwo) {
+          const temp = previousShownCard;
+          setTimeout(() => {
+            temp.classList.remove('flip');
+            card.classList.remove('flip');
+          }, 1000);
+        }
+        previousShownCard = undefined;
+      }
+    }
+  });
+
+  cards.push(card);
+
+  container.appendChild(card);
 }
