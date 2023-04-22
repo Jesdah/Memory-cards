2d771c6a9ce0fbc256f9e74c708cc1173a4c3ac9
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-04-22 18:54:00 +0000

    trying to make the shuffle work

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,38 @@ let icons= ['fa-solid fa-image',
 'fa-solid fa-headphones',
 'fa-solid fa-plane',
 'fa-solid fa-bicycle',
+'fa-solid fa-feather',
+'fa-solid fa-image',
+'fa-solid fa-star',
+'fa-solid fa-heart',
+'fa-solid fa-ghost',
+'fa-solid fa-headphones',
+'fa-solid fa-plane',
+'fa-solid fa-bicycle',
 'fa-solid fa-feather'];
 
 icons.push(...icons);
 
-// Shufle icons.
-function shuffleCards(icons) {
-  cards=icons.length;
-  for (let i = 0; i < cards; i--) {
-    randomIcon=Math.Floor(Math.random()*(i+1));
-    temp=icons[i]
-    icons[i] = icons[randomIcon]
-    icons[randomIcon] = temp
-    
+// Shufle icons. The Fisher-Yates algorith.
+const shuffleCards= icons => {
+  for (let i = icons.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i+1));
+    const temp= icons[i];
+    icons[i]=icons[j];
+    icons[j]= temp;
   }
-  return icons;
-};
+}
+function shuffle (){
+  mathed=0;
+  disabeDeck= false;
+  cardOne=cardTwo="";
+  icons.sort(() => Math.random() > 0.5 ? 1: -1);
+  cards.forEach((card,i) => {
+    card.classList.remove("flipcard");
+    let symbol= card.querySelector("back");
+    card.addEventListener("click", flipCard);
+  })
+}
 
 
 // I made this function with help of this turorial:https://jefferson-cuartas.medium.com/how-to-create-a-flip-card-effect-using-javascript-767dd945210c
