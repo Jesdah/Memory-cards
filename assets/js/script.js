5af5d41d5be6895541272aab0051827ceb1ec7e0
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-04-22 21:16:00 +0000

    trying to make the shuffle function to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,39 @@
-const card= document.querySelectorAll('.play-card');
+const playCard = document.querySelectorAll('.play-card');
 const cardslength = 16;
 let cards = [];
 
 
-let flippedCard=false;
-let cardOne,cardTwo;
+let flippedCard = false;
+let cardOne, cardTwo;
 
-let icons= ['fa-solid fa-image',
-'fa-solid fa-star',
-'fa-solid fa-heart',
-'fa-solid fa-ghost',
-'fa-solid fa-headphones',
-'fa-solid fa-plane',
-'fa-solid fa-bicycle',
-'fa-solid fa-feather',
-'fa-solid fa-image',
-'fa-solid fa-star',
-'fa-solid fa-heart',
-'fa-solid fa-ghost',
-'fa-solid fa-headphones',
-'fa-solid fa-plane',
-'fa-solid fa-bicycle',
-'fa-solid fa-feather'];
+let icons = ['fa-solid fa-image',
+  'fa-solid fa-star',
+  'fa-solid fa-heart',
+  'fa-solid fa-ghost',
+  'fa-solid fa-headphones',
+  'fa-solid fa-plane',
+  'fa-solid fa-bicycle',
+  'fa-solid fa-feather',
+];
 
 icons.push(...icons);
+// Shufle icons. copied from: https://github.com/swapnilsparsh/30DaysOfJavaScript/blob/master/27%20-%20Memory%20Matching%20Game/script.js
+for (let i = 0; i < 100; i++) {
+  const idx1 = Math.floor(Math.random() * icons.length);
+  const idx2 = Math.floor(Math.random() * icons.length);
 
-// Shufle icons. The Fisher-Yates algorith.
-const shuffleCards= icons => {
-  for (let i = icons.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i+1));
-    const temp= icons[i];
-    icons[i]=icons[j];
-    icons[j]= temp;
-  }
+  const temp = icons[idx1];
+  icons[idx1] = icons[idx2];
+  icons[idx2] = temp;
 }
-function shuffle (){
-  mathed=0;
-  disabeDeck= false;
-  cardOne=cardTwo="";
-  icons.sort(() => Math.random() > 0.5 ? 1: -1);
-  cards.forEach((card,i) => {
-    card.classList.remove("flipcard");
-    let symbol= card.querySelector("back");
-    card.addEventListener("click", flipCard);
-  })
-}
+console.log(icons);
+
 
 
 // I made this function with help of this turorial:https://jefferson-cuartas.medium.com/how-to-create-a-flip-card-effect-using-javascript-767dd945210c
 
-function flipCard () {
-this.classList.toggle('flipcard');
+function flipCard() {
+  this.classList.toggle('flipcard');
 
 }
-card.forEach(card => card.addEventListener('click', flipCard));
+playCard.forEach(card => card.addEventListener('click', flipCard));
