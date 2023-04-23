7cec4623dd1ec57dcecab3542e6fccea4e4c2c74
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-04-23 08:50:17 +0000

    add a working match function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,9 @@ let icons = ['fa-solid fa-image',
 ];
 
 icons.push(...icons);
+
 // Shufle icons. copied from: https://github.com/swapnilsparsh/30DaysOfJavaScript/blob/master/27%20-%20Memory%20Matching%20Game/script.js
+
 for (let i = 0; i < 100; i++) {
   const idx1 = Math.floor(Math.random() * icons.length);
   const idx2 = Math.floor(Math.random() * icons.length);
@@ -31,9 +33,43 @@ console.log(icons);
 
 
 // I made this function with help of this turorial:https://jefferson-cuartas.medium.com/how-to-create-a-flip-card-effect-using-javascript-767dd945210c
+/*
+function flipCard() 
+  this.classList.toggle('flipcard');*/
+let first;
+let second;
+let matchCardCounter=0;
 
-function flipCard() {
-  this.classList.toggle('flipcard');
+playCard.forEach(function(card) {
+  card.addEventListener('click', function() {
+    if (!first && !second) {
+    first=card;
+    card.classList.add('flipcard');
+    } else if (first && !second) {
+      second=card;
+      card.classList.add('flipcard')
 
-}
+      if (first.innerHTML=== second.innerHTML) {
+        first=null;
+        second=null;
+        alert ("match");
+        matchCardCounter++;
+
+        if (matchCardCounter >=8) {
+          alert ("All cards match!");
+        } else {
+          first.classList.remove('flipcard');
+          second.classList.remove('flipcard');
+          setTimeout(()=> {
+            first.classList.remove('flipcard');
+            second.classList.remove('flipcard');
+            first=null;
+            second=null;
+          }, 2000);
+        }
+      }
+    }
+  })
+} ) 
+
 playCard.forEach(card => card.addEventListener('click', flipCard));
